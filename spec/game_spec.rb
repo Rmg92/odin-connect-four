0c1bed1cecb104023dcc16a05fc1a63355c5c8b9
Ricardo <ricardomg92@hotmail.com>
2022-08-08 18:51:18 +0200

    Create tests for the player creation on the Game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require './lib/game'
+
+describe Game do
+  subject(:create_players) { described_class.new }
+
+  before do
+    allow(create_players).to receive(:puts)
+    allow(create_players).to receive(:gets).and_return('Jarvis')
+    create_players.create_players
+  end
+
+  describe '#create_players' do
+    it 'returns Jarvis as player 1 name' do
+      expect(create_players.instance_variable_get(:@player_one).name).to eq('Jarvis')
+    end
+
+    it "returns \u26AA as player 1 piece" do
+      expect(create_players.instance_variable_get(:@player_one).piece).to eq("\u26AA")
+    end
+
+    it 'returns Jarvis as player 2 name' do
+      expect(create_players.instance_variable_get(:@player_two).name).to eq('Jarvis')
+    end
+
+    it "returns \u26AB as player 2 piece" do
+      expect(create_players.instance_variable_get(:@player_two).piece).to eq("\u26AB")
+    end
+  end
+end
