d261c48be29aae5b77e9ac300611700dbb32fab2
Ricardo <ricardomg92@hotmail.com>
2022-08-08 18:52:04 +0200

    Create #create_players method which creates two new players for the game

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'player'
+
+# contains all the logic needed for the game to be played
+class Game
+  def create_players
+    puts 'Insert player 1 name?'
+    @player_one = Player.new(gets, "\u26AA")
+    puts 'Insert player 2 name?'
+    @player_two = Player.new(gets, "\u26AB")
+  end
+end
