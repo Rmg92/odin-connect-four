2a4eace32d970cc3168daa11d8af08b62158f161
Ricardo <ricardomg92@hotmail.com>
2022-08-21 10:23:23 +0200

    Create #drop_piece method
    It drops a piece on the right column and row.
    A column is given to the method who then searches the next empty row on
    the column and changes the value of the row to the player piece.

--- lib/board.rb
+++ lib/board.rb
@@ -2,11 +2,24 @@
 
 # Creates a new board for the game
 class Board
+  attr_reader :board
+
   def initialize
     @board = create_board
   end
 
   def create_board
     Array.new(7) { Array.new(6) }
+  end
+
+  def drop_piece(column, piece)
+    row = find_empty_row(column)
+    @board[column - 1][row] = piece
+  end
+
+  def find_empty_row(column)
+    row = 6
+    row -= 1 until (@board[column - 1][row]).nil?
+    row
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,15 +4,34 @@ require './lib/board'
 
 describe Board do
   subject(:new_board) { described_class.new }
-  let(:board) { new_board.instance_variable_get(:@board) }
 
-  context 'when board is initialized' do
-    it 'returns an array of length 7 columns' do
-      expect(board.length).to eq(7)
+  describe '#create_board' do
+    context 'when board is initialized' do
+      it 'returns an array of length 7 columns' do
+        expect(new_board.board.length).to eq(7)
+      end
+
+      it 'returns an array of length 6 rows' do
+        expect(new_board.board[0].length).to eq(6)
+      end
     end
+  end
 
-    it 'returns an array of length 6 rows' do
-      expect(board[0].length).to eq(6)
+  describe '#drop_piece' do
+    context 'when the player choses a column' do
+      before do
+        new_board.drop_piece(3, "\u26AA")
+      end
+
+      it 'drops a piece at the end of an empty column' do
+        # expect { new_board.drop_piece(3, "\u26AA") }.to change(new_board.board[2][6]).from(nil).to("\u26AA")
+        expect(new_board.board[2][6]).to eq("\u26AA")
+      end
+
+      it 'drops a piece on top of another piece' do
+        new_board.drop_piece(3, "\u26AA")
+        expect(new_board.board[2][5]).to eq("\u26AA")
+      end
     end
   end
 end
